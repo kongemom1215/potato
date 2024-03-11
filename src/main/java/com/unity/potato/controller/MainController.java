6c1ea589fe4ede355fdc9f986b7836cb271674f5
Juhye <kongemom1215@gmail.com>
2024-03-12 05:21:39 +0900

    로그인 페이지 html 추가

--- src/main/java/com/unity/potato/controller/MainController.java
+++ src/main/java/com/unity/potato/controller/MainController.java
@@ -21,4 +21,8 @@ public class MainController {
         return "requireLoginMain";
     }
 
+    @RequestMapping("/community/login")
+    public String goLoginPage(){
+        return "login";
+    }
 }
